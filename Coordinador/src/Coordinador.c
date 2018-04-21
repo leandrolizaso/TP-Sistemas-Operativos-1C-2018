37a0c0f21debcea7bb1a2c93c4b7d5fcc99ec1da
David <davidperez@est.frba.utn.edu.ar>
2018-04-21 15:29:18 -0300

    Esqueleto de proceso con comunicacion

--- Coordinador/src/Coordinador.c
+++ Coordinador/src/Coordinador.c
@@ -10,8 +10,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sockets.h>
 
-int main(void) {
-	puts("!!!Soy el proceso coordinador!!!");
+int main(int argc, char* argv[]) {
+	char* puerto = "9999";
+	char* handshake = "coord_says_hi";
+	char* mensaje_recibido;
+
+	puts("Levantando..");
+	int server_socket = crear_server(puerto);
+
+	puts("Esperando conexion");
+	int client_socket = aceptar_conexion(server_socket);
+
+	puts("Conexion recibida, enviando handshake");
+	int chars_sent = enviar_string(client_socket,handshake);
+	if (strlen(handshake) != chars_sent) {
+		puts("Opa! algo sucedio y no pude mandar handshake");
+		return EXIT_FAILURE;
+	}
+	mensaje_recibido = recibir_string(client_socket);
+	printf("Acabo de recibir %s... Eso quiere decir que estamos?\n",mensaje_recibido);
+
+	free(mensaje_recibido);
+
 	return EXIT_SUCCESS;
 }
