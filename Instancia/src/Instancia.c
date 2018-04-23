f8d8aa12ad75c93ec4bc0788161d0411d9db86e3
Alexander Ortega <alexorte03@gmail.com>
2018-04-23 17:35:31 -0300

    Comunicacion inicial de instancia-coordinador.

--- Instancia/src/Instancia.c
+++ Instancia/src/Instancia.c
@@ -10,8 +10,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sockets.h>
 
 int main(void) {
+	char* mensaje_recibido;
+
 	puts("!!!Soy el proceso instancia!!!");
+
+
+	int conexion_coordinador = conectar_a_server("127.0.0.1","9999");
+	mensaje_recibido = recibir_string(conexion_coordinador);
+	printf("recibi \"%s\" del coordinador",mensaje_recibido);
+
+	enviar_string(conexion_coordinador, "soy la instancia ameo");
+
+	free(mensaje_recibido);
 	return EXIT_SUCCESS;
 }
