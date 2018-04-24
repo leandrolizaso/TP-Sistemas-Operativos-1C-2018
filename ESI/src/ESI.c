216d8695475c61de3c3c28bc338c531e46061506
Fernando Arellano <f.arellano919@gmail.com>
2018-04-24 00:44:29 -0300

    Agrego inicializar ESI

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -8,10 +8,69 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "ESI.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
 	puts("!!!Soy el proceso ESI!!!");
+
 	return EXIT_SUCCESS;
+}
+
+void inicializar(char* path){
+
+	// Creo log
+
+	logger = log_create("esi.log","ESI",false,LOG_LEVEL_TRACE);
+
+	// Cargo la configuración
+
+	config_aux = config_create(path);
+
+	if(config_has_property(config_aux, "IP_COORDINADOR")){
+		config.ip_coordinador = config_get_string_value(config_aux, "IP_COORDINADOR");
+	}else{
+		log_error(logger, "No se encuentra la ip del Coordinador");
+		finalizar();
+		exit(EXIT_FAILURE);
+	}
+
+	if(config_has_property(config_aux, "PUERTO_COORDINADOR")){
+		config.puerto_coordinador = config_get_string_value(config_aux, "PUERTO_COORDINADOR");
+	}else{
+		log_error(logger, "No se encuentra el puerto del Coordinador");
+		finalizar();
+		exit(EXIT_FAILURE);
+	}
+
+	if(config_has_property(config_aux, "IP_PLANIFICADOR")){
+		config.ip_planificador = config_get_string_value(config_aux, "IP_PLANIFICADOR");
+	}else{
+		log_error(logger, "No se encuentra la ip del Planificador");
+		finalizar();
+		exit(EXIT_FAILURE);
+	}
+
+	if(config_has_property(config_aux, "PUERTO_PLANIFICADOR")){
+		config.puerto_planificador = config_get_string_value(config_aux, "PUERTO_PLANIFICADOR");
+	}else{
+		log_error(logger, "No se encuentra el puerto del Planificador");
+		finalizar();
+		exit(EXIT_FAILURE);
+	}
+
+	log_info(logger, "Se cargó exitosamente la configuración");
+
+	// Me conecto al Coordinador y Planificador
+
+	socket_coordinador = conectar_a_server(config.ip_coordinador, config.puerto_coordinador);
+	socket_planificador = conectar_a_server(config.ip_planificador, config.puerto_planificador);
+
+	log_info(logger, "Conexión exitosa al Coordinador y Planificador");
+}
+
+void finalizar(){
+	log_info(logger, "Fin ejecución");
+	config_destroy(config_aux);
+	log_destroy(logger);
 }
