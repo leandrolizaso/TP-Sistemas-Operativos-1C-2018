6aae13eba1b64cbd52078a7fd5d38ef43427b5a7
Alexander Ortega <alexorte03@gmail.com>
2018-04-27 15:51:13 -0300

    agregado protocolo a instancia

--- Instancia/src/Instancia.c
+++ Instancia/src/Instancia.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sockets.h>
+#include <protocolo.h>
 
 int main(void) {
 	char* mensaje_recibido;
@@ -20,9 +21,13 @@ int main(void) {
 
 	int conexion_coordinador = conectar_a_server("127.0.0.1","9999");
 	mensaje_recibido = recibir_string(conexion_coordinador);
-	printf("recibi \"%s\" del coordinador",mensaje_recibido);
-
-	enviar_string(conexion_coordinador, "soy la instancia ameo");
+	if(conexion_coordinador == HANDSHAKE_COORDINADOR){
+		printf("recibi \"%s\" del coordinador",mensaje_recibido);
+		enviar_string(conexion_coordinador, HANDSHAKE_ESI);
+	}else{
+		printf("recibi \"%s\" del coordinador otro mesaje por error",mensaje_recibido);
+		enviar_string(conexion_coordinador, STRING_SENT);
+	}
 
 	free(mensaje_recibido);
 	return EXIT_SUCCESS;
