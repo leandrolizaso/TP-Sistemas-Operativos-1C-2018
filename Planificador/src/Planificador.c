9e3b1ddd83f01a0156222f9e176ff9b117968c53
cristianEguez <ceguez@est.frba.utn.edu.ar>
2018-04-28 03:48:29 -0300

    se agrega comunicacion con coordinador

--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -12,8 +12,14 @@
 #include <stdlib.h>
 #include <sockets.h>
 #include <pthread.h>
+#include "protocolo.h"
+
+char* ip_coordinador = "127.0.0.1";
+int puerto_coordinador = 8000;
+int socket_coordinador = conectar_a_server((char *)ip_coordinador,(char *) puerto_coordinador);
 
 char* puerto = "8010";
+
 
 void agregar_espacio(char* buffer){
 	int i=0;
@@ -45,7 +51,6 @@ void* consola(void* no_use){
 
 		if(string_equals_ignore_case(token[0],"pausar")){
 			//pausar(token[1]);
-
 		}
 
 		if(string_equals_ignore_case(token[0],"continuar")){
@@ -89,7 +94,6 @@ int main(void) {
 
 	empezar_comunicacion_ESI();
 
-
 	pthread_t consola_thread;
 
 	if(pthread_create(&consola_thread, NULL, &consola,NULL)){
@@ -103,7 +107,6 @@ int main(void) {
 		return 0;
 
 	}
-
 
 	return EXIT_SUCCESS;
 }
@@ -123,5 +126,24 @@ void empezar_comunicacion_ESI(){
 	printf("Recibi: %s \n",recib);
 
 	free(recib);
+
+}
+
+
+void empezar_comunicacion_coordinador(){
+
+	int result = enviar(socket_coordinador,HANDSHAKE_PLANIFICADOR,0,NULL);
+
+	t_paquete* paquete = recibir(socket_coordinador);
+
+	if(paquete->codigo_operacion == HANDSHAKE_COORDINADOR)
+		puts(" Conexion con el coordinador exitosa.");
+	destruir_paquete(paquete);
+}
+
+void enviar_paquete_coordinador(){
+
+	char * mensaje = "S1N V0C4L3S P4P4 XD";
+	enviar(socket_coordinador, 500, strlen(mensaje), (void *)mensaje); // STRING_SENT = 500 ; en el protocolo del coord :3
 
 }
