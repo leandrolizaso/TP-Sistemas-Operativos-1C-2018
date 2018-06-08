9d3cca355c2e6cd6906357c7697c3b575bdec371
David <davidperez@est.frba.utn.edu.ar>
2018-06-07 23:42:11 -0300

    fixes en serializacion

--- lib/protocolo.c
+++ lib/protocolo.c
@@ -3,18 +3,19 @@
 #include "protocolo.h"
 
 int sizeof_clavevalor(t_clavevalor cv) {
-	return sizeof(int) * 2 + strlen(cv.clave) + strlen(cv.valor);
+	return sizeof(int)*2 + (strlen(cv.clave) + strlen(cv.valor) + 2) * sizeof(char);
 }
 
 void* serializar_clavevalor(t_clavevalor cv) {
-	int l_clave = strlen(cv.clave);
-	int l_valor = strlen(cv.valor);
-	void* buffer = malloc(sizeof_clavevalor(cv));
+	int l_clave = strlen(cv.clave)+1;
+	int l_valor = strlen(cv.valor)+1;
+	int size_cv = sizeof_clavevalor(cv);
+	void* buffer = malloc(size_cv);
 
 	memcpy(buffer, &l_clave, sizeof(int));
 	memcpy(buffer + sizeof(int), &l_valor, sizeof(int));
-	memcpy(buffer + sizeof(int) * 2, cv.clave, l_clave);
-	memcpy(buffer + sizeof(int) * 2 + l_clave, cv.valor, l_valor);
+	memcpy(buffer + sizeof(int) * 2, cv.clave, l_clave*sizeof(char));
+	memcpy(buffer + sizeof(int) * 2 + l_clave*sizeof(char), cv.valor, l_valor*sizeof(char));
 
 	return buffer;
 }
@@ -22,7 +23,7 @@ void* serializar_clavevalor(t_clavevalor cv) {
 t_clavevalor deserializar_clavevalor(void* buffer) {
 	int* l_clave = buffer;
 	int* l_valor = l_clave + 1;
-	char* clave = (char*) l_valor + 1;
+	char* clave = (char*) (l_valor + 1);
 	char* valor = clave + *l_clave;
 
 	t_clavevalor cv;
@@ -40,7 +41,10 @@ void* serializar_mensaje_esi(t_mensaje_esi mensaje_esi) {
 
 	memcpy(buffer, &mensaje_esi.id_esi, sizeof(int));
 	memcpy(buffer + sizeof(int), &mensaje_esi.keyword, sizeof(int));
-	memcpy(buffer + sizeof(int) * 2, serializar_clavevalor(mensaje_esi.clave_valor), sizeof_clavevalor(mensaje_esi.clave_valor));
+
+	void* subbuffer = serializar_clavevalor(mensaje_esi.clave_valor);
+	memcpy(buffer + sizeof(int) * 2, subbuffer, sizeof_clavevalor(mensaje_esi.clave_valor));
+	free(subbuffer);
 
 	return buffer;
 }
@@ -48,7 +52,7 @@ void* serializar_mensaje_esi(t_mensaje_esi mensaje_esi) {
 t_mensaje_esi deserializar_mensaje_esi(void* buffer) {
 	int* id_esi = buffer;
 	int* keyword = id_esi+1;
-	t_clavevalor* clave_valor = (t_clavevalor*) keyword+1;
+	t_clavevalor* clave_valor = (t_clavevalor*) (keyword+1);
 
 	t_mensaje_esi mensaje_esi;
 	mensaje_esi.id_esi = *id_esi;
