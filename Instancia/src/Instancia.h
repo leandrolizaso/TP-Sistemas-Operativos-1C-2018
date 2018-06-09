c8c395e52b3008a05bf6bd816390c87d7a276b4d
Cam Palomino <camilarpalomino@gmail.com>
2018-06-09 14:56:15 -0300

    update instancia.h

--- Instancia/src/Instancia.h
+++ Instancia/src/Instancia.h
@@ -10,7 +10,8 @@
 #include <pelao/protocolo.h>
 #include <commons/config.h>
 #include <commons/log.h>
-#include "Instancia.h"
+#include <commons/collections/list.h>
+
 
 #define CFG_IP  "ip_coordinador"
 #define CFG_PORT  "port_coordinador"
@@ -19,7 +20,7 @@
 #define CFG_NAME_INST  "name_instancia"
 #define CFG_INTERVAL  "interval"
 #define CFG_TAMANIO (tamanio)
-
+#define CFG_CAPACIDAD (capacidad)
 
 //Leer arcivo de configuracion
 t_config* leer_config(int argc, char* argv[]);
@@ -28,17 +29,21 @@ void finalizar( t_config* config,t_log* logger);
 void inicializar(t_config* config,t_log* logger);
 
 //estructuras
-typedef struct entrada* t_entrada;
+typedef struct entrada t_entrada;
 typedef int sig_atomic_t;
 typedef struct t_paquete* paquete;
-typedef struct Nodo* Nodo;
+typedef struct Nodo t_Nodo;
+typedef struct t_list listas;
 
-int destruir_entrada (struct t_entrada* bloque);
-void push (Nodo* pila, t_entrada* valores);
-void crearEntrada (struct t_clavevalor* claveValor);
+int destruir_entrada (t_entrada* bloque);
+void push (t_Nodo* pila, t_entrada* valores);
+void crearEntrada (t_clavevalor* claveValor);
 void verificarOperacion (int codOperacion, struct t_clavevalor* claveValor);
 void dump (int intervalo);
-
-#endif // INSTANCIA_H_INCLUDED
+void algoritmoCircular(t_entrada* bloque, t_list listaAlmacenamiento);
+void esValorAtomico(t_entrada* lista);
+void almacenarClaves (t_entrada* lista);
+void guardarClaves(listaAlmacenamiento );
+void buscarEntrada(struct t_entrada* listaAlmacenamiento, t_clavevalor* info);
 
 #endif // INSTANCIA_H_INCLUDED
