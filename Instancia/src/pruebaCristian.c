f59681a9b63975bf8f2344038dffb4b4d45e4222
Leandro <llizaso@est.frba.utn.edu.ar>
2018-06-09 01:29:57 -0300

    Prueba responde positivamente, fixes en la misma y agrego strelen_null al protocolo

--- Instancia/src/pruebaCristian.c
+++ Instancia/src/pruebaCristian.c
@@ -1,33 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+#include <commons/collections/list.h>
 #include "pruebaCristian.h"
 
-int main(int argc, char** argv){
-	inicializar();
+int main(int argc,char* argv[]){
+	inicializar(argv[1]);
 	atenderConexiones();
 	liberarRecursos();
 	return EXIT_SUCCESS;
 }
 
-void inicializar(){
+void inicializar(char* path){
 	crearLog();
-	levantarConfig();
+	levantarConfig(path);
 	conectarCoordinador();
 	crearTablaIndices();
 	crearMemoria();
 }
 
-void levantarConfig(){
-	t_config* config_aux = config_create("inst.cfg");
+void levantarConfig(char* path) {
 
-	// Tener en cuenta config_has_property(config_aux,"key"); para validar si esta todo...
+	config_aux = config_create(path);
 
-	config.algoritmo = config_get_string_value(config_aux,"distribution_replacement");
-	config.intervalo= config_get_double_value(config_aux,"interval");
-	config.ip_coordinador = config_get_string_value(config_aux,"ip_coordinador");
-	config.nombre = config_get_string_value(config_aux,"name_instancia");
-	config.point_mount = config_get_string_value(config_aux,"point_mount");
-	config.puerto_coordinador = config_get_int_value(config_aux,"port_coordinador");
+	if (config_has_property(config_aux, "ip_coordinador")) {
+		config.ip_coordinador = config_get_string_value(config_aux,"ip_coordinador");
+	} else {
+		log_error(logger, "No se encuentra la ip del Coordinador");
+	}
 
-	config_destroy(config_aux);
+	if (config_has_property(config_aux, "port_coordinador")) {
+		config.puerto_coordinador = config_get_string_value(config_aux,"port_coordinador");
+	} else {
+		log_error(logger, "No se encuentra port_coordinador");
+	}
+
+	if (config_has_property(config_aux, "distribution_replacement")) {
+		config.algoritmo = config_get_string_value(config_aux,"distribution_replacement");
+	} else {
+		log_error(logger, "No se encuentra distribution_replacement");
+	}
+
+	if (config_has_property(config_aux, "point_mount")) {
+		config.point_mount = config_get_string_value(config_aux,"point_mount");
+	} else {
+		log_error(logger, "No se encuentra point_mount");
+	}
+
+	if (config_has_property(config_aux, "name_instancia")) {
+		config.nombre= config_get_string_value(config_aux,"name_instancia");
+	} else {
+		log_error(logger, "No se encuentra name_instancia");
+	}
+
+	if (config_has_property(config_aux, "interval")) {
+		config.intervalo= config_get_int_value(config_aux,"interval");
+	} else {
+		log_error(logger, "No se encuentra interval");
+	}
+
+
+	log_info(logger,"Se cargó exitosamente la configuración");
 
 }
 
@@ -45,10 +78,9 @@ void conectarCoordinador() {
 	if (paquete->codigo_operacion != HANDSHAKE_COORDINADOR) {
 		log_error(logger, "Falló el handshake con el Coordinador");
 		destruir_paquete(paquete);
-		morir();
 	} else {
 		cantidad_entradas = (int)paquete->data;
-		tamanio_entrada = (int)(paquete->data+sizeof(int));
+		tamanio_entradas= (int)(paquete->data+sizeof(int));
 		log_info(logger,"Handshake exitoso con el Coordinador");
 		destruir_paquete(paquete);
 	}
@@ -59,47 +91,51 @@ void atenderConexiones(){
 	paquete = recibir(socket_coordinador);
 	char* error;
 	int imRunning = 1;
+
 	while(imRunning){
 
 		switch(paquete->codigo_operacion){
-		case SAVE_CLAVE:
+		case SAVE_CLAVE:{
 			t_clavevalor claveValor = deserializar_clavevalor(paquete->data);
-			if (tengoLaClave(claveValor->valor)) {
+			if (tengoLaClave(claveValor.clave)) {
 				guardarPisandoClaveValor(claveValor);
 			} else {
 				guardarClaveValor(claveValor);
 			}
 			notificar_coordinador(0);
-		break;
-
-		//DUMP_CLAVE    7777: nana nana nana nana
-		case 7777:
-			char* clave = malloc(sizeof(char)*paquete->tamanio);
-			strcpy(clave,paquete->data);
-			t_espacio_memoria* memory = tengoLaClave(claveValor->valor);
-
-			// mmap para guardar el valor <<--- ia bere khe ago
-
-			t_indice* indice = list_get(tablaIndices, memory->id);
-			indice->idOcupante = -1;
-
-		break;
-
-		default:
+			// en cada guardar deberia tener un notificar y depende del error/exito notificar
+			// ahora notifico cero, para que todinho salga bien
+		break;}
+		case DUMP_CLAVE:{
+			char* clave = strdup(paquete->data);
+			t_espacio_memoria* memory = conseguirEspacioMemoria(clave);
+			if(memory == NULL){
+				//notificar_coordinador(3); // <-- 3 = ERROR: se quiere hacer STORE de una clave que no se posee.
+			}else{
+				// mmap para guardar el valor <<--- ia bere khe ago
+				t_indice* indice = list_get(tablaIndices, memory->id);
+				indice->idOcupante = -1;
+			}
+			notificar_coordinador(0);
+			//para que de bien
+		break;}
+		default:{
 			error = string_from_format("El codigo de operación %d no es válido", paquete->codigo_operacion);
 			log_error(logger, error);
 			free(error);
 			destruir_paquete(paquete);
+			}
 		}
 
-
+	destruir_paquete(paquete);
+	paquete = recibir(socket_coordinador);
 	}
 
 }
 
 
 void notificar_coordinador(int respuesta){
-	enviar(socket_coordinador,RESPUESTA_INTANCIA,sizeof(int),respuesta);
+	enviar(socket_coordinador,RESPUESTA_INTANCIA,sizeof(int),(void*)respuesta);
 }
 
 // FORMAS DE GUARDAR
@@ -110,15 +146,26 @@ void notificar_coordinador(int respuesta){
 //			 si: guardo
 //			 no: compacto <-- por ahora no. Entonces ahora es mandar un error por falta de espacio
 
-void guardarPisandoClaveValor(claveValor){
+void guardarPisandoClaveValor(t_clavevalor claveValor){
 	// tener en cuenta si el nuevo valor ocupa +o- Entradas
 }
 
-void guardarClaveValor(claveValor){
+void guardarClaveValor(t_clavevalor claveValor){
 }
 
-
 // Creacion y Destruccion
+
+void finalizar(){
+	liberarRecursos();
+	exit(EXIT_SUCCESS);
+}
+
+void liberarRecursos(){
+	config_destroy(config_aux);
+	log_destroy(logger);
+	destruirMemoria();
+	destruirTablaIndices();
+}
 
 void crearTablaIndices(){
 	tablaIndices = list_create();
@@ -139,15 +186,20 @@ void destruirMemoria(){
 	list_destroy(memoria);
 }
 
-void desruirTablaIndices(){
+void destruirTablaIndices(){
 	list_destroy(tablaIndices);
 }
 
-
-
 // PARA LISTAS
 
-t_espacio_memoria* tengoLaClave(char* clave){
+bool tengoLaClave(char* clave){
+	bool contieneClave(void* unaParteDeMemoria){
+		return string_equals_ignore_case(((t_espacio_memoria*)unaParteDeMemoria)->clave,clave);
+	}
+	return list_any_satisfy(memoria,&contieneClave);
+}
+
+t_espacio_memoria* conseguirEspacioMemoria(char* clave){
 	bool contieneClave(void* unaParteDeMemoria){
 		return string_equals_ignore_case(((t_espacio_memoria*)unaParteDeMemoria)->clave,clave);
 	}
--- Instancia/src/pruebaCristian.h
+++ Instancia/src/pruebaCristian.h
@@ -8,11 +8,9 @@
 #ifndef SRC_PRUEBACRISTIAN_H_
 #define SRC_PRUEBACRISTIAN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <commons/log.h>
 #include <pelao/sockets.h>
 #include <pelao/protocolo.h>
-#include <commons/config.h>
 
 typedef struct {
 	char* clave;
@@ -28,25 +26,44 @@ typedef struct{
 
 typedef struct {
 	char* ip_coordinador;
-	int puerto_coordinador;
+	char* puerto_coordinador;
 	char* algoritmo;
 	char* point_mount;
 	char* nombre;
 	int intervalo;
 } t_config_instancia;
 
-t_indice* tablaIndices;
-t_espacio_memoria* memoria;
+t_list* tablaIndices;
+t_list* memoria;
 t_config_instancia config;
 t_log* logger;
+t_config* config_aux;
 
 int socket_coordinador;
 int cantidad_entradas;
 int tamanio_entradas;
 
+
+void notificar_coordinador(int respuesta);
+void guardarPisandoClaveValor(t_clavevalor claveValor);
+void guardarClaveValor(t_clavevalor claveValor);
+
+bool tengoLaClave(char* clave);
+t_espacio_memoria* conseguirEspacioMemoria(char* clave);
+
+void inicializar();
+void crearLog();
 void levantarConfig();
 void conectarCoordinador();
 void atenderConexiones();
+
+int entradasQueOcupa(char* valor);
+
+void destruirMemoria();
+void destruirTablaIndices();
 void liberarRecursos();
+
+void crearTablaIndices();
+void crearMemoria();
 
 #endif /* SRC_PRUEBACRISTIAN_H_ */
