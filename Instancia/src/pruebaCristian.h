defaf947b64bd2e132b9a493ea152e7cbf62467e
Cam Palomino <camilarpalomino@gmail.com>
2018-06-24 14:25:49 -0300

    se agrega funcion dump

--- Instancia/src/pruebaCristian.h
+++ Instancia/src/pruebaCristian.h
@@ -98,4 +98,6 @@ void escribirEnArchivo(t_espacio_memoria* espacio);
 char* extraerValor(t_espacio_memoria* espacio);
 void mostrar(t_espacio_memoria* espacio);
 
+void dump (t_espacio_memoria* tabla);
+
 #endif /* SRC_PRUEBACRISTIAN_H_ */
