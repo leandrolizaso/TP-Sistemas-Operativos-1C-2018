d5105c39811a6f21396db25f9171ca1f19f4db33
Leandro Lizaso <llizaso@est.frba.utn.edu.ar>
2018-07-13 21:51:57 -0300

    Fix handshake

--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -280,6 +280,7 @@ int procesar_mensaje(int socket) {
 	switch (paquete->codigo_operacion) {
 
 	case HANDSHAKE_ESI: {
+		usleep(300000);
 		proceso_esi_t* nuevo_esi = nuevo_processo_esi(socket);
 		enviar(socket, HANDSHAKE_PLANIFICADOR, sizeof(int),&(nuevo_esi->ID));
 		sem_wait(m_ready);
